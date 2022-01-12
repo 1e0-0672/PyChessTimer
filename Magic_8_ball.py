7e2fa42df70992e037fbefcdba83f2403fb38758
1e0-0672 <80422321+1e0-0672@users.noreply.github.com>
2022-01-11 22:24:13 -0800

    Add files via upload
    I added code to it and used the random library.

---
+++ Magic_8_ball.py
@@ -0,0 +1,9 @@
+import random
+
+Answers = ["Certainly", "No way", "Try again", "Maybe", "There is a high chance", "The chances are not looking good", "Nobody cares"]
+
+input("Ask a question")
+
+Chosen_Anwer =random.choice(Answers[0, 1, 2, 3, 4, 5, 6])
+
+print (Chosen_Anwer)
